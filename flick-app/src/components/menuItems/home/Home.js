43175f68a4525af8a52066f036e6835cea4b1db6
kulakash1 <akashkulshrestha7@gmail.com>
2024-06-09 01:36:38 +0530

    Added menuItems

---
+++ flick-app/src/components/menuItems/home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MenuItems from '../MenuItems'
+
+function Home() {
+  return (
+    <div>
+        <div className=' bg-[#352D39] h-full'>
+        <MenuItems />
+            </div>
+        </div>
+  )
+}
+
+export default Home
