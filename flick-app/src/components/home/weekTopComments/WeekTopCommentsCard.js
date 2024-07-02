b6bdf44e980b9ddc61853d65cd63901614a787f1
kulakash1 <akashkulshrestha7@gmail.com>
2024-07-02 15:20:07 +0530

    Added containers in Top comment of week section

--- flick-app/src/components/home/weekTopComments/WeekTopCommentsCard.js
+++ flick-app/src/components/home/weekTopComments/WeekTopCommentsCard.js
@@ -1,36 +1,69 @@
-import React from 'react'
+import React from "react";
+import { FaLongArrowAltRight } from "react-icons/fa";
 
 export const WeekTopCommentsCard = () => {
   return (
     <div>
-        <div className="flex flex-row">
-            <div>
-                <img src='flex.jpg' alt='Top Comments Image' />
-            </div>
-            <div className="flex flex-col">
-                <div>
-                    <h2>Movie Title</h2>
-                </div>
-                <div>
-                    <p>Movie Year</p>
-                </div>
-                <div className="flex flex-row">
-                    <div>
-                        <img src='CircleImg.jpg' alt='Circle Image' />
-                    </div>
-                    <div>
-                        <p>Critic Name</p>
-                    </div>
-                    <div>
-                        <p>Rating</p>
-                    </div>
-                </div>
-                <div>
-                    <p> Comment Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi numquam eum explicabo magnam quidem cumque laborum rerum commodi dicta vitae?</p>
-                </div>
-                
-            </div>
+      <div className="flex flex-row">
+        <div>
+          <img src="flex.jpg" alt="Top Comments Image" />
         </div>
+        <div className="flex flex-col">
+          <div>
+            <h2>Movie Title</h2>
+          </div>
+          <div>
+            <p>Movie Year</p>
+          </div>
+          <div className="flex flex-row">
+            <div>
+              <img src="CircleImg.jpg" alt="Circle Image" />
+            </div>
+            <div>
+              <p>Critic Name</p>
+            </div>
+            <div>
+              <p>Rating</p>
+            </div>
+          </div>
+          <div>
+            <p>
+              {" "}
+              Comment Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Sequi numquam eum explicabo magnam quidem cumque laborum rerum
+              commodi dicta vitae?
+            </p>
+          </div>
+          <div className="flex flex-row justify-end">
+            <button onClick={() => window.open("www.mqsoftware.com", "_blank")}>
+              <div className="flex flex-row">
+                ...see more
+                <div className="flex flex-col justify-center ml-1">
+                  <FaLongArrowAltRight />
+                </div>
+              </div>
+            </button>
+          </div>
+          <div className="flex flex-row">
+            <div className="flex flex-row">
+                <div>
+                <FaLongArrowAltRight />
+                </div>
+                <div>
+                    <h2>Comments</h2>
+                </div>
+            </div>
+            <div className="flex flex-row ml-4">
+                <div>
+                <FaLongArrowAltRight />
+                </div>
+                <div>
+                    <h2>Likes</h2>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
